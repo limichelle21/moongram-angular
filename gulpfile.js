b27b4d4e95195e4529dde32c50ea55b1cba24197
Michelle Li <limichelle21@gmail.com>
2017-08-02 21:04:25 -0700

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,8 @@
-gulp.task('serve-prod', function() {
-var history = require('connect-history-api-fallback');
-return plugins.connect.server({
-root: 'build',
-port: process.env.PORT || 5000,
-livereload: false,
-middleware: function(connect, opt) {
-return [history()];
-}
-});
+
+gulp.task('serveprod', function() {
+  connect.server({
+    root: 'src',
+    port: process.env.PORT || 5000, // localhost:5000
+    livereload: false
+  });
 });
